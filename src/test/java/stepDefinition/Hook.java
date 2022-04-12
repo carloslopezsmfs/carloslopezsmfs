77aae8cc382497e9a37e75120b56a92de5d2df77
Carlos Lopez Villen <carlos.lopez.contractor@smallworldfs.com>
2022-04-12 10:21:42 +0200

    Test without sandbox and update test

--- src/test/java/stepDefinition/Hook.java
+++ src/test/java/stepDefinition/Hook.java
@@ -19,7 +19,7 @@ public class Hook {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--headless");
+        //options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.navigate().to("https://qa.oswfs.com/NexxarGenWeb/");
         driver.manage().window().maximize();
--- src/test/java/stepDefinition/TestPage.java
+++ src/test/java/stepDefinition/TestPage.java
@@ -5,20 +5,44 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import static stepDefinition.Hook.driver;
+
 public class TestPage {
 
-    public void test() {
-        WebElement username = Hook.driver.findElement(By.name("username"));
-        username.sendKeys("audrey_400313");
-        WebElement password = Hook.driver.findElement(By.id("password"));
-        password.sendKeys("P@ssword1");
-        WebElement submitBtn = Hook.driver.findElement(By.xpath("/html/body/form/article/section[2]/section/div[1]/div/input"));
+    private final String menuFrameOmnex2 = "menu";
+
+    public void test() throws InterruptedException {
+        /*String url = Hook.driver.getCurrentUrl();
+        WebElement inputElement = Hook.driver.findElement(By.name("q"));
+        inputElement.sendKeys("test");
+        WebElement submitBtn = Hook.driver.findElement(By.name("btnK"));
         submitBtn.click();
-        Assert.assertTrue(Hook.driver.findElement(By.id("menuTypeAheadField")).isDisplayed());
+        String urlNew = Hook.driver.getCurrentUrl();
+        Assert.assertNotEquals(url, urlNew);*/
+        WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys("audrey_400313");
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("P@ssword1");
+        WebElement submitBtn = driver.findElement(By.xpath("/html/body/form/article/section[2]/section/div[1]/div/input"));
+        submitBtn.click();
+        Thread.sleep(1000);
+        goToMenu();
+        Assert.assertTrue(driver.findElement(By.id("menu_whoAmI_myUserSettings")).isDisplayed());
+    }
+
+    private void changeFrame(String frameName) {
+        // Get the default content
+        driver.switchTo().defaultContent();
+        // set the FRAME with the Menu
+        driver.switchTo().frame(frameName);
+    }
+
+    public void goToMenu() {
+        changeFrame(menuFrameOmnex2);
     }
 
     @Given("I log in")
-    public void iLogIn() {
+    public void iLogIn() throws InterruptedException {
         test();
     }
 }
