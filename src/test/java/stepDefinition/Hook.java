0706b7176e8f0942ed9166f8214df547cc44befe
Carlos Lopez Villen <carlos.lopez.contractor@smallworldfs.com>
2022-04-12 12:10:05 +0200

    Test without headless

--- src/test/java/stepDefinition/Hook.java
+++ src/test/java/stepDefinition/Hook.java
@@ -7,8 +7,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.concurrent.TimeUnit;
-
 public class Hook {
 
     public static WebDriver driver;
@@ -17,17 +15,14 @@ public class Hook {
     public static void setUp() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
         //options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.navigate().to("https://qa.oswfs.com/NexxarGenWeb/");
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(120, TimeUnit.MILLISECONDS);
     }
 
     @After
-    public static void tearDown() {
+    public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
